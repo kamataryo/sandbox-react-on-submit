ba695f4f2c1c6af465015cdcf8f751c759b7114d
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-10-20 14:32:51 +0900

    Fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <form onSubmit={() => alert(1)}>
+          <input type="submit" value={'submit'} />
+          <button type="submit">submit</button>
+        </form>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
